c505758c43f77a0f9fb324116afbd84c3c29cabc
Katja Süss <k.suess@rohberg.ch>
2022-09-21 09:32:48 +0200

    Remove invalid python_requires.

--- setup.py
+++ setup.py
@@ -28,6 +28,9 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Operating System :: OS Independent",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
     ],
@@ -47,7 +50,6 @@ setup(
     package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
-    python_requires="==2.7, >=3.6",
     install_requires=[
         "setuptools",
         # -*- Extra requirements: -*-
