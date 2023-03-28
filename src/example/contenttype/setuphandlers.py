ffee650484428e6b73ee715db35aa2d647ab62c5
Katja Süss <k.suess@rohberg.ch>
2023-03-28 23:46:26 +0200

    Create many examples and many relations

--- src/example/contenttype/setuphandlers.py
+++ src/example/contenttype/setuphandlers.py
@@ -1,6 +1,13 @@
-# -*- coding: utf-8 -*-
+from example.contenttype.manyexamples.staff_names import employee_names
+from plone import api
 from Products.CMFPlone.interfaces import INonInstallable
 from zope.interface import implementer
+
+
+import logging
+import random
+
+logger = logging.getLogger("example.contenttype")
 
 
 @implementer(INonInstallable)
@@ -20,3 +27,52 @@ def post_install(context):
 def uninstall(context):
     """Uninstall script"""
     # Do something at the end of the uninstallation of this package.
+
+
+def create_many_examples(context):
+    number_of_employees = 500
+    logger.info("*** Create many examples.")
+    portal = api.portal.get()
+    staff = api.content.create(type="Document", title="Example Staff", container=portal)
+    employees_folder = api.content.create(
+        type="Document", title="Employees", container=staff
+    )
+    supervisors_folder = api.content.create(
+        type="Document", title="Supervisors", container=staff
+    )
+
+    ens = employee_names["items"]
+
+    employees = []
+    supervisors = []
+    for i in range(number_of_employees):
+        title = f'Employee {ens[i]["first_name"]} {ens[i]["last_name"]}'
+        employee = api.content.create(
+            type="example", title=title, container=employees_folder
+        )
+        api.content.transition(employee, "publish")
+        employees.append(employee)
+        title = f'Employee {ens[i]["first_name_supervisor"]} {ens[i]["last_name_supervisor"]}'
+        supervisor = api.content.create(
+            type="example", title=title, container=supervisors_folder
+        )
+        api.content.transition(supervisor, "publish")
+        supervisors.append(supervisor)
+
+    for i in range(number_of_employees):
+        # first relation
+        rint = random.randint(0, number_of_employees - 1)
+        api.relation.create(
+            source=employees[i],
+            target=supervisors[rint],
+            relationship="relationlist_field_named_staticcatalogvocabulary",
+        )
+        # second relation
+        rint = random.randint(0, number_of_employees - 1)
+        api.relation.create(
+            source=employees[i],
+            target=supervisors[rint],
+            relationship="relationlist_field_named_staticcatalogvocabulary",
+        )
+
+    logger.info("*** Many examples created and related.")
