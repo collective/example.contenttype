86bc7e363653a348a8aeba0b15c45613499603f7
Katja Süss <k.suess@rohberg.ch>
2023-03-29 11:58:51 +0200

    Update setuphandlers.py

--- src/example/contenttype/setuphandlers.py
+++ src/example/contenttype/setuphandlers.py
@@ -30,52 +30,56 @@ def uninstall(context):
 
 
 def create_many_examples(context):
-    number_of_employees = 300
+    number_of_employees = 10
     logger.info("*** Create many examples.")
-    portal = api.portal.get()
-    staff = api.content.create(type="Document", title="Example Staff", container=portal)
-    employees_folder = api.content.create(
-        type="Document", title="Employees", container=staff
-    )
-    supervisors_folder = api.content.create(
-        type="Document", title="Supervisors", container=staff
-    )
-
-    ens = employee_names["items"]
-
-    employees = []
-    supervisors = []
-    for i in range(number_of_employees):
-        # Employee
-        title = f'Employee {ens[i]["first_name"]} {ens[i]["last_name"]}'
-        employee = api.content.create(
-            type="example", title=title, container=employees_folder
+    staff = api.content.get(path="/example-staff")
+    if not staff:
+        portal = api.portal.get()
+        staff = api.content.create(
+            type="Document", title="Example Staff", container=portal
         )
-        api.content.transition(employee, "publish")
-        employees.append(employee)
-
-        # Supervisor
-        title = f'Supervisor {ens[i]["first_name_supervisor"]} {ens[i]["last_name_supervisor"]}'
-        supervisor = api.content.create(
-            type="example", title=title, container=supervisors_folder
+        employees_folder = api.content.create(
+            type="Document", title="Employees", container=staff
         )
-        api.content.transition(supervisor, "publish")
-        supervisors.append(supervisor)
-
-    for i in range(number_of_employees):
-        # first relation
-        rint = random.randint(0, number_of_employees - 1)
-        api.relation.create(
-            source=employees[i],
-            target=supervisors[rint],
-            relationship="relationlist_field_named_staticcatalogvocabulary",
-        )
-        # second relation
-        rint = random.randint(0, number_of_employees - 1)
-        api.relation.create(
-            source=employees[i],
-            target=supervisors[rint],
-            relationship="relationlist_field_named_staticcatalogvocabulary",
+        supervisors_folder = api.content.create(
+            type="Document", title="Supervisors", container=staff
         )
 
-    logger.info("*** Many examples created and related.")
+        ens = employee_names["items"]
+
+        employees = []
+        supervisors = []
+        for i in range(number_of_employees):
+            # Employee
+            title = f'Employee {ens[i]["first_name"]} {ens[i]["last_name"]}'
+            employee = api.content.create(
+                type="example", title=title, container=employees_folder
+            )
+            api.content.transition(employee, "publish")
+            employees.append(employee)
+
+            # Supervisor
+            title = f'Supervisor {ens[i]["first_name_supervisor"]} {ens[i]["last_name_supervisor"]}'
+            supervisor = api.content.create(
+                type="example", title=title, container=supervisors_folder
+            )
+            api.content.transition(supervisor, "publish")
+            supervisors.append(supervisor)
+
+        for i in range(number_of_employees):
+            # first relation
+            rint = random.randint(0, number_of_employees - 1)
+            api.relation.create(
+                source=employees[i],
+                target=supervisors[rint],
+                relationship="relationlist_field_named_staticcatalogvocabulary",
+            )
+            # second relation
+            rint = random.randint(0, number_of_employees - 1)
+            api.relation.create(
+                source=employees[i],
+                target=supervisors[rint],
+                relationship="relationlist_field_named_staticcatalogvocabulary",
+            )
+
+        logger.info("*** Many examples created and related.")
