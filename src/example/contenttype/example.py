028e9f01c0576ae3a78a17f56e5c54daddf966d0
Peter Mathis <peter.mathis@kombinat.at>
2025-05-24 12:52:47 +0200

    Add TreeVocabulary field and fix imports for Plone 6.1

--- src/example/contenttype/example.py
+++ src/example/contenttype/example.py
@@ -2,31 +2,32 @@
 from plone.app.textfield import RichText
 from plone.app.vocabularies.catalog import CatalogSource
 from plone.app.vocabularies.catalog import StaticCatalogVocabulary
-from plone.app.z3cform.widget import AjaxSelectFieldWidget
-from plone.app.z3cform.widget import RelatedItemsFieldWidget
-from plone.app.z3cform.widget import SelectFieldWidget
 from plone.autoform import directives
 from plone.dexterity.content import Container
-
 from plone.namedfile.field import NamedBlobFile
 from plone.namedfile.field import NamedBlobImage
 from plone.schema import Email
-
-# from plone.schema import (
-#     Dict,
-# )  # take Dict field from plone.schema to use the widget attribute
 from plone.supermodel import model
 from plone.supermodel.directives import fieldset
 from plone.supermodel.directives import primary
-
 from z3c.form.browser.checkbox import CheckBoxFieldWidget
 from z3c.form.browser.radio import RadioFieldWidget
 from z3c.relationfield.schema import RelationChoice
 from z3c.relationfield.schema import RelationList
 from zope import schema
 from zope.interface import implementer
-
 from zope.interface import Interface
+
+try:
+    # Plone 6.1
+    from plone.app.z3cform.widgets.relateditems import RelatedItemsFieldWidget
+    from plone.app.z3cform.widgets.select import AjaxSelectFieldWidget
+    from plone.app.z3cform.widgets.select import Select2FieldWidget
+except ImportError:
+    # Plone 6.0
+    from plone.app.z3cform.widget import RelatedItemsFieldWidget
+    from plone.app.z3cform.widget import AjaxSelectFieldWidget
+    from plone.app.z3cform.widget import SelectFieldWidget as Select2FieldWidget
 
 
 class IExample(model.Schema):
@@ -59,6 +60,7 @@ class IExample(model.Schema):
             "choice_field",
             "choice_field_radio",
             "choice_field_select",
+            "choice_field_treevocabulary",
             "choice_field_voc",
             "list_field",
             "list_field_checkbox",
@@ -194,11 +196,29 @@ class IExample(model.Schema):
         required=False,
     )
 
-    directives.widget(choice_field_select=SelectFieldWidget)
+    directives.widget(choice_field_select=Select2FieldWidget)
     choice_field_select = schema.Choice(
         title="Choicefield with select2 widget",
         description="zope.schema.Choice",
         vocabulary="plone.app.vocabularies.PortalTypes",
+        required=False,
+    )
+
+    directives.widget(choice_field_treevocabulary=Select2FieldWidget)
+    choice_field_treevocabulary = schema.Choice(
+        title="Choicefield with TreeVocabulary",
+        vocabulary=schema.vocabulary.TreeVocabulary.fromDict(
+            {
+                ("foo_group", "Foo Group"): {
+                    ("bar_group", "Bar Group"): {},
+                    ("qux_group", "Qux Group"): {},
+                },
+                ("corge_group", "Corge Group"): {
+                    ("grault_group", "Grault Group"): {},
+                    ("garply_group", "Garply Group"): {},
+                },
+            }
+        ),
         required=False,
     )
 
@@ -225,7 +245,7 @@ class IExample(model.Schema):
         default=[],
     )
 
-    directives.widget(list_field_select=SelectFieldWidget)
+    directives.widget(list_field_select=Select2FieldWidget)
     list_field_select = schema.List(
         title="List field with select widget",
         description="zope.schema.List",
@@ -417,7 +437,7 @@ class IExample(model.Schema):
     # )
     # directives.widget(
     #     "relationchoice_field_select",
-    #     SelectFieldWidget,
+    #     Select2FieldWidget,
     # )
 
     # relationchoice_field_radio = RelationChoice(
@@ -445,7 +465,7 @@ class IExample(model.Schema):
     )
     directives.widget(
         "relationlist_field_select",
-        SelectFieldWidget,
+        Select2FieldWidget,
         pattern_options={
             "closeOnSelect": False,  # Select2 option to leave dropdown open for multiple selection
         },
@@ -598,7 +618,7 @@ class IExample(model.Schema):
     # )
     # directives.widget(
     #     "uuid_choice_field_select",
-    #     SelectFieldWidget,
+    #     Select2FieldWidget,
     # )
 
     # uuid_choice_field_radio = schema.Choice(
@@ -626,7 +646,7 @@ class IExample(model.Schema):
     )
     directives.widget(
         "uuid_list_field_select",
-        SelectFieldWidget,
+        Select2FieldWidget,
         pattern_options={
             "closeOnSelect": False,  # Select2 option to leave dropdown open for multiple selection
         },
